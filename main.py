5155372127c8ba6b5b1344eacf0e3945b672fd51
azimov-dev <a.azimov@tuit.uz>
2026-09-10 01:01:19 +0500

    Added 'game over' feature when the player is collided with asteroid

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
+
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import PLAYER_RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -36,6 +38,11 @@ def main():
 
         screen.fill("black")
         updatable.update(dt)
+        for asteroid1 in asteroids:
+            if player.collides_with(asteroid1):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for drawable1 in drawable:
             drawable1.draw(screen)
         pygame.display.flip()
